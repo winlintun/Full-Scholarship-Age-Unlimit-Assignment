64051c126b63db10aeed0f27d3bfe8c0a72c56fb
iceface <32415733+winlintun@users.noreply.github.com>
2022-09-15 23:03:22 +0630

    first assignment (nodjs server & html file)
    first assignment

---
+++ Web&Technology/first_assignment/script.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const fs = require('fs').promises
+
+const hostname = "127.0.0.1"
+const port = 8000;
+
+let indexFile;
+
+const server = http.createServer((req, res) => {
+    fs.readFile(__dirname + "/index.html")
+    .then(contents => {
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(200);
+        res.end(contents);
+    })
+    .catch(err => {
+        res.writeHead(500);
+        res.end(err);
+        return;
+    });
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
